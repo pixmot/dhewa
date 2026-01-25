b0c61a69943ef55af0ede6e6f5cae92dbe1dab0e
Amogh Rijal <xamoghx@gmail.com>
2026-01-25 13:44:05 +0545

    Add txn editor keyboard height preference key

--- Ordinatio/Infrastructure/PreferencesKeys.swift
+++ Ordinatio/Infrastructure/PreferencesKeys.swift
@@ -2,4 +2,5 @@ enum PreferencesKeys {
     static let hasOnboarded = "hasOnboarded"
     static let defaultCurrencyCode = "defaultCurrencyCode"
     static let activeHouseholdId = "activeHouseholdId"
+    static let transactionEditorKeyboardHeight = "transactionEditorKeyboardHeight"
 }
